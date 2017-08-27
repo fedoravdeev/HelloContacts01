0442f2d2f5172d8e09b08b03304df3472922c6eb
FA <fedor.avdeev@gmail.com>
2017-08-28 01:58:43 +0300

    fix jdbc

--- src/com/example/servlets/HelloContacts.java
+++ src/com/example/servlets/HelloContacts.java
@@ -23,40 +23,43 @@ public class HelloContacts extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//String helloContacts = "a";
+		String helloContacts = "a";
 		response.setContentType("text/html"); 
 		
 		PrintWriter out = response.getWriter(); 
 												
+		
+		try {
+
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost:5432/Contacts";
+			String login = "postgres";
+			String password = "1234";
+			Connection con = DriverManager.getConnection(url, login, password);
+			try {
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT * FROM CONTACTS");
+
+				while (rs.next()) {
+					String str = rs.getString("id") + ":" + rs.getString(2);
+					helloContacts = "<br>"+ helloContacts + str;
+					System.out.println("Contact:" + str);
+				}
+				rs.close();
+				stmt.close();
+			} finally {
+				con.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		out.write("<!DOCTYPE html>\n" + 										
 				"<html>\n" 
 				+ "<head><title>Hello Contacts.</title></head>\n"
 				+ "<body bgcolor=\"#fdf5e6\">\n" + "<h1>Note:</h1>\n"
-				+ "<p>Hello Contacts was created via servlet</p>\n" + "</body></html>");
-		
- /*       try {
-            Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/contacts";
-            String login = "postgres";
-            String password = "1234";
-            Connection con = DriverManager.getConnection(url, login, password);
-            try {
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM CONTACTS");
-                
-                while (rs.next()) {
-                    String str = rs.getString("contact_id") + ":" + rs.getString(2);                    
-                    helloContacts = "<br> Contact:" + str;
-                }
-                rs.close();
-                stmt.close();
-            } finally {
-                con.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }	*/	
-		
+				+ "<p>Hello Contacts was created via servlet</p>\n" +helloContacts+ "</body></html>");
+
 		
 
 	}
