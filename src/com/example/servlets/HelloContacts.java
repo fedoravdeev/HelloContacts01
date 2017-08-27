68a0e4e7e39885253ceccc129e946b910ab38470
FA <fedor.avdeev@gmail.com>
2017-08-27 23:32:19 +0300

    fix doget

--- src/com/example/servlets/HelloContacts.java
+++ src/com/example/servlets/HelloContacts.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello01")
+//@WebServlet("/hello01")
 public class HelloContacts extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html"); 
 												
